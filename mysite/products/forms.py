f8192857f67d2ec0696a718b9cb65e50ada90113
otchlan <raf.szuwalski@gmail.com>
2019-06-11 21:47:54 +0200

    BoTrzeba

---
+++ mysite/products/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class productForm(forms.Form)
+    your_title = forms.CharField(label=Title, max_length= 50)
+
+    #Ciekawe czy ktoś to przeczyta i się pod nosem zaśmieje:)
+
+    #bledy wola do forms.py i view.py trzeba przerbić
--- mysite/products/views.py
+++ mysite/products/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from .models import Product, SuperProduct
 
+from .forms import productForm
+from .models import Product, SuperProduct
+from django.http import HttpResponseRedirect
+
+#from .forms import nameForm
 
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
@@ -21,3 +25,13 @@ def super_product_detail_view(request):
         'sobject': objS
     }
     return render(request, "s_product_detail.html", context)
+
+def get_title(request):
+    if request.method == 'POST':
+        form = productForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('/products/')
+    else:
+        form = productForm()
+
+    return render(request, "product_detail.html", {'form': form})
